89384554f19aebb9ec9af044b80edaad30af880c
HuyHungM <nguyenhuyhung161111@gmail.com>
2024-04-13 01:00:43 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,8 @@
 module.exports = (client) => {
   client.once("ready", () => {
     client.user.setPresence({
-      activities: [{ name: " làm người yêu bạn", type: "Competing" }],
-      status: "online",
+      activities: [{ name: "trò làm người yêu bạn", type: "PLAYING" }],
+      status: "idle",
     });
     console.log(`🟢 [LIVE]: BOT đã đăng nhập với tên ${client.user.tag}!`);
   });
