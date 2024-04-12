8077b86dfa7cceb69264cb877602fe352ffb2981
HuyHungM <nguyenhuyhung161111@gmail.com>
2024-04-13 00:48:50 +0700

    keep alive

---
+++ src/modules/dashboard.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+
+module.exports = (client) => {
+  const app = express();
+
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+
+  app.listen(process.env.DASHBOARD_PORT, () =>
+    console.log(
+      `🟢 [ALIVE] Dashboard được mở ở cổng ${process.env.DASHBOARD_PORT}`
+    )
+  );
+};
