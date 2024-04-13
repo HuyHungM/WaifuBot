9e3878b27272ccd6887e11347d36d8158a2495dc
HuyHungM <nguyenhuyhung161111@gmail.com>
2024-04-13 21:16:31 +0700

    Update antiCrash.js

--- src/modules/antiCrash.js
+++ src/modules/antiCrash.js
@@ -11,9 +11,4 @@ module.exports = (client) => {
     console.log("🔴 [AntiCrash]: Uncaught Exception/Catch (MONITOR)");
     console.log(err);
   });
-  process.on("multipleResolves", (type, promise, reason) => {
-    console.log("🔴 [AntiCrash]: Multiple Resolves");
-    if (reason === "Error: Cannot perform IP discovery - socket closed") return;
-    console.log(type);
-  });
 };
