e41dd909a4056d5ba281efc8534bdc4fa1d98931
HuyHungM <nguyenhuyhung161111@gmail.com>
2024-05-18 14:16:42 +0700

    update dashboard

--- src/dashboard/routes/api.js
+++ src/dashboard/routes/api.js
@@ -5,18 +5,18 @@ app.get("/api/get/manifest.json", (req, res) => {
   const manifest = {
     name: `${client.user.username} - Bot cho Discord`,
     short_name: client.user.username,
-    start_url: "/",
+    start_url: "/servers",
     display: "standalone",
     background_color: "#18181b",
-    theme_color: "#5868fa",
+    theme_color: "#000000",
     icons: [
       {
-        src: client.user.displayAvatarURL({ extension: "png" }),
+        src: client.user.displayAvatarURL({ extension: "png", size: 256 }),
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: client.user.displayAvatarURL({ extension: "png" }),
+        src: client.user.displayAvatarURL({ extension: "png", size: 512 }),
         sizes: "512x512",
         type: "image/png",
       },
