dd3dc80c3f64bad0966c72977ce98b0087fb01e1
HuyHungM <nguyenhuyhung161111@gmail.com>
2024-05-18 14:07:14 +0700

    update dashboard

--- src/dashboard/routes/api.js
+++ src/dashboard/routes/api.js
@@ -1,12 +1,29 @@
 const app = require("express").Router();
+const client = require("../../app");
 
-app.get("/api/get/user", (req, res) => {
-  const user = req.user;
-  if (user) {
-    return res.status(200).json({ user: user });
-  } else {
-    return res.status(404).json({ user: null });
-  }
+app.get("/api/get/manifest.json", (req, res) => {
+  const manifest = {
+    name: `${client.user.username} - Bot cho Discord`,
+    short_name: client.user.username,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#18181b",
+    theme_color: "#5868fa",
+    icons: [
+      {
+        src: client.user.displayAvatarURL({ extension: "png" }),
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: client.user.displayAvatarURL({ extension: "png" }),
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+
+  res.json(manifest);
 });
 
 module.exports = app;
